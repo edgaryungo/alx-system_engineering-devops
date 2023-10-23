1156c7b5ee3637880cee27f175bf43b21adf71d4
Edgar <eddy8471@gmail.com>
2023-10-23 14:25:16 +0300

    Final commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,23 +3,40 @@
 import requests
 import sys
 
+def get_employee_todo_list(employee_id):
+    """ Script that uses JSONPlaceholder API to get information about employee """
+    try:
+        # Make a GET request to the API endpoint
+        response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+        response.raise_for_status()
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+        todos = response.json()
 
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
+        # Calculate the number of completed and total tasks
+        total_tasks = len(todos)
+        completed_tasks = sum(1 for todo in todos if todo['completed'])
 
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
+        # Get the employee name (assuming user data is available)
+        user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+        user_response.raise_for_status()
+        user_data = user_response.json()
+        employee_name = user_data['name']
 
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+        print(f'Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):')
+
+        # Print the titles of completed tasks
+        for todo in todos:
+            if todo['completed']:
+                print(f'\t{todo["title"]}')
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list(employee_id)
