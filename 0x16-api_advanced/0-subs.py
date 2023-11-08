42df38f109bfba3fe4c8c5533f8047d335b68ef9
Edgar <eddy8471@gmail.com>
2023-11-08 10:51:55 +0300

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,37 +1,27 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
-    # Set a custom User-Agent to avoid Too Many Requests errors
+    """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
 
     headers = {
         'User-Agent': u_agent
     }
 
-    # Reddit API URL to get the subreddit information
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    try:
-        # Send a GET request to the Reddit API
-        response = requests.get(url, headers=headers)
-
-        # Check if the response status code is 200 (OK)
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            
-            # Extract the number of subscribers from the response
-            subscribers = data['data']['subscribers']
-            
-            return subscribers
-        else:
-            # Handle invalid subreddit or other errors
-            return 0
-    except requests.exceptions.RequestException:
-        # Handle request exceptions, e.g., network issues
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-
-# Example usage:
-# subreddit_name = 'learnpython'
-# subscribers_count = number_of_subscribers(subreddit_name)
-# print(f'The subreddit "{subreddit_name}" has {subscribers_count} subscribers.')
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
